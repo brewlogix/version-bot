ee5f6ae8dec9eade2301eb60f6557acde4b671d2
Ian Cameron Lyles <ian.lyles@brewlogix.com>
2024-03-16 14:35:58 -0700

    Update index.ts (#1)

--- index.ts
+++ index.ts
@@ -65,9 +65,9 @@ Merging this PR will not cause a version bump for any packages. If these changes
 
 ${getReleasePlanMessage(releasePlan)}
 
-[Click here to learn what changesets are, and how to add one](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md).
+// [Click here to learn what changesets are, and how to add one](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md).
 
-[Click here if you're a maintainer who wants to add a changeset to this PR](${addChangesetUrl})
+[Add a changeset to this PR](${addChangesetUrl})
 
 `;
 
@@ -83,9 +83,9 @@ Latest commit: ${commitSha}
 
 ${getReleasePlanMessage(releasePlan)}
 
-Not sure what this means? [Click here  to learn what changesets are](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md).
+// Not sure what this means? [Click here  to learn what changesets are](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md).
 
-[Click here if you're a maintainer who wants to add another changeset to this PR](${addChangesetUrl})
+[Click here to add another changeset to this PR](${addChangesetUrl})
 
 `;
 
